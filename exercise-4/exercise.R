32406861b1f9f65cedd56e57b1bd2a32bae41847
Rowel Danao <danaorow@uw.edu>
2018-04-05 23:52:17 -0700

    Exercise Completed

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,12 +4,18 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function(word1, word2) {
+  difference <- abs(nchar(word1) - nchar(word2)) # Finds difference of 2 characters
+  min_length <- min(nchar(word1), nchar(word2)); # Finds the numerical value of smallest string
+  difference >= min_length # States if the difference is greater than the minimum.
+} 
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-
+is_twice_as_long("kiwi", "watermelon")
+is_twice_as_long("watermelon", "kiwi")
+is_twice_as_long("acai", "kiwi")
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -17,8 +23,20 @@
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
-
+describe_difference <- function(word1, word2) {
+  diff <- nchar(word1) - nchar(word2)
+  if(diff > 0) {
+    sentence <- paste("Your first string is longer by", diff, "characters")
+  } else if(diff < 0) {
+    sentence <- paste("Your second string is longer by", -diff, "characters")
+  } else {
+    sentence <- "Your strings are the same length!"
+  }
+  sentence
+}
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
-
+describe_difference("kiwi", "watermelon")
+describe_difference("watermelon", "kiwi")
+describe_difference("acai", "kiwi")
